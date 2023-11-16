62b9bae3d79a5db4ef7450b70978fc8a36c99462
Roman Litvinenko <kyubey300@Mac-Roman.local>
2023-11-16 17:01:38 +0600

    Repo created

---
+++ iOS_development_1/iOS_development_1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_development_1App.swift
+//  iOS_development_1
+//
+//  Created by Роман on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_development_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
